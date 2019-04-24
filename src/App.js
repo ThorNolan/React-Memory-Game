53632cea7c2aa223aebce0ecf5f6c19653883faf
Thor Nolan <tnolan5764@gmail.com>
2019-04-23 21:40:14 -0700

    Set up some colors in colors.json, App is now a stateful component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import Container from './components/Container';
+import Col from './components/Col';
+import Row from './components/Row';
+import Wrapper from './components/Wrapper';
+import Navbar from './components/Navbar';
+import ColorTile from './components/ColorTile';
+import Logo from './components/Logo'
+import Colors from './colors.json'
 import './App.css';
 
-function App() {
-  return (
-    <div>
+class App extends Component {
 
-    </div>
-  );
+  // Set the initial state of the app 
+  state = {
+
+  }
+  
+  // Render my display for the main page of the application 
+  render() {
+    return (
+      <Wrapper>
+      <div>
+  
+      </div>
+      </Wrapper>
+    );
+  }
 };
 
 export default App;
--- src/components/ColorTile/ColorTile.js
+++ src/components/ColorTile/ColorTile.js
@@ -2,7 +2,15 @@ import React from "react";
 import "./ColorTile.css";
 
 const ColorTile = props => {
-
+    <div
+    className="card"
+    value={props.id}
+    onClick={() => props.HandleClick(props.id)}
+    >
+      <div className="tile-container">
+        
+      </div>  
+    </div>
 };
 
 export default ColorTile;
