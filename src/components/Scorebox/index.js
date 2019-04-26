2dc75ae788a3de5f6ae5a5912c27b946639f46c3
Thor Nolan <tnolan5764@gmail.com>
2019-04-25 17:14:32 -0700

    Adjusted some styles for scorebox, added some media queries for better responsive text sizing

--- src/components/Scorebox/index.js
+++ src/components/Scorebox/index.js
@@ -4,7 +4,7 @@ import "./style.css";
 const Scorebox = props => {
   return (
     <div className="scorebox container mx-auto">
-        <h3 className="text-center">Click the colors, but only once! Click the same color twice and your score will reset.</h3>
+        <h3 className="text-center">Try to click each color, but only once! Click any color twice and your score will reset</h3>
 
         <p className="text-center font-weight-bold">Current Score: {props.currentScore} | High Score: {props.highScore}</p>
     </div>
