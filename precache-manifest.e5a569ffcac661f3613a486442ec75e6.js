4738d085b3a0953bf7609692825287f25b19905e
Thor Nolan <tnolan5764@gmail.com>
2019-04-25 19:05:58 -0700

    Updates

---
+++ precache-manifest.e5a569ffcac661f3613a486442ec75e6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9cedcb71ab1522f7472d0bfb5203feb5",
+    "url": "/React-Memory-Game/index.html"
+  },
+  {
+    "revision": "8237e67fa85fc9f7f18d",
+    "url": "/React-Memory-Game/static/css/main.eb08ec6b.chunk.css"
+  },
+  {
+    "revision": "dd0ed99a380f8d225736",
+    "url": "/React-Memory-Game/static/js/2.884c28c3.chunk.js"
+  },
+  {
+    "revision": "8237e67fa85fc9f7f18d",
+    "url": "/React-Memory-Game/static/js/main.745bfbfa.chunk.js"
+  },
+  {
+    "revision": "146a3181e76c06b70ec8",
+    "url": "/React-Memory-Game/static/js/runtime~main.39b1e4df.js"
+  }
+]);
